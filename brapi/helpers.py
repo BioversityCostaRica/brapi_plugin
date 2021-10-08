a170de86ff6c073a1ef5a013d3de9a1d8c4d7ad4
qlands <cquiros@qlands.com>
2021-10-08 11:29:20 -0600

    Integrate Brapi into create a project

--- brapi/helpers.py
+++ brapi/helpers.py
@@ -1,2 +1,44 @@
 def check_integration(user, project):
     return True
+
+
+breed_base_servers = [
+    {
+        "code": "test",
+        "name": "Test server",
+        "url": "https://test-server.brapi.org/brapi/v2/",
+    },
+    {
+        "code": "cassavabase",
+        "name": "NextGen Cassava Project",
+        "url": "https://cassavabase.org/brapi/v2/",
+    },
+    {
+        "code": "yambase",
+        "name": "Africa Yam Project",
+        "url": "https://yambase.org/brapi/v2/",
+    },
+]
+
+
+def get_servers():
+    return breed_base_servers
+
+
+def get_server_url(server_code):
+    for a_server in breed_base_servers:
+        if a_server["code"] is server_code:
+            return a_server["url"]
+    return None
+
+
+def get_crops():
+    return [{"code": "maize", "name": "Maize"}, {"code": "beans", "name": "Beans"}]
+
+
+def crop_exist(crop_code):
+    crops = get_crops()
+    for a_crop in crops:
+        if a_crop["code"] == crop_code:
+            return True
+    return False
--- brapi/plugin.py
+++ brapi/plugin.py
@@ -1,7 +1,13 @@
 import climmob.plugins as plugins
 import climmob.plugins.utilities as u
-from views import BRAPIServersView
-from .helpers import check_integration
+from .views import BRAPIServersView
+from .helpers import (
+    check_integration,
+    get_servers,
+    get_crops,
+    get_server_url,
+    crop_exist,
+)
 
 
 class BrAPI(plugins.SingletonPlugin):
@@ -9,6 +15,7 @@ class BrAPI(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfig)
     plugins.implements(plugins.ISchema)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IProject)
 
     def before_mapping(self, config):
         # We don't add any routes before the host application
@@ -28,10 +35,37 @@ class BrAPI(plugins.SingletonPlugin):
 
     def update_schema(self, config):
         return [
-            u.addFieldToProjectSchema("link_breedbase", "Link to a BreedBase"),
+            u.addFieldToProjectSchema("breedbase_link", "Link to a BreedBase"),
+            u.addFieldToProjectSchema("breedbase_crop", "Main crop of the breedBase"),
             u.addFieldToProjectSchema("breedbase_server", "BreedBase Server"),
-            u.addFieldToProjectSchema("breedbase_api", "BreedBase API version"),
+            u.addFieldToProjectSchema("breedbase_url", "BreedBase Server URL"),
         ]
 
     def get_helpers(self):
-        return {"check_integration": check_integration}
+        return {
+            "check_integration": check_integration,
+            "get_servers": get_servers,
+            "get_crops": get_crops,
+        }
+
+    def before_adding_project(self, request, user, project_data):
+        _ = request.translate
+        if "breedbase_link" in project_data.keys():
+            project_data["breedbase_link"] = 1
+            if project_data.get("breedbase_crop", None) is None:
+                return False, _("You need to specify a BreedBase crop"), project_data
+            if project_data.get("breedbase_server", None) is None:
+                return False, _("You need to specify a BreedBase server"), project_data
+            print("*********************88")
+            print(project_data["breedbase_server"])
+            print("*********************88")
+            server_url = get_server_url(project_data["breedbase_server"])
+            if server_url is None:
+                return False, _("Server not found"), project_data
+            if not crop_exist(project_data["breedbase_crop"]):
+                return False, _("Crop not found"), project_data
+            project_data["breedbase_url"] = server_url
+        return True, "", project_data
+
+    def after_adding_project(self, request, user, project_data):
+        pass
